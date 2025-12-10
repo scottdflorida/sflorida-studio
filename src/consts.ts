64b8d33d26b2c28657573427b7da3cc45dfb7755
Scott Florida <scott.d.florida@gmail.com>
2025-12-10 04:36:43 -0800

    Add dark forest green theme and site branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'sflorida.studio';
+export const SITE_DESCRIPTION = 'Dev Blog for AI-native Indie Game Studio';
